edc1d6b6f384e30be673651484ec8c2d7e5bb85f
Soham Jana <sohamjana2k25@gmail.com>
2025-10-19 12:41:27 +0530

    Update modelsave.py
    changed random state to get better accuracy

--- modelsave.py
+++ modelsave.py
@@ -9,7 +9,7 @@ df = pd.read_csv('placement_with_rating.csv')
 X = df.iloc[:, :-1].values
 Y = df.iloc[:, -1].values
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=32)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=50)
 
 model = linear_regression(0.0001, 50)
 model.fit(X_train, Y_train)
